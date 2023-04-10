15f3f2de0cfaf546fc327031fb5996ef970755f0
toluhunter <toluhunter19@gmail.com>
2023-04-10 14:08:14 +0100

    Added Aws Region

--- account/signals.py
+++ account/signals.py
@@ -1,6 +1,7 @@
 import boto3
 
 from django.db.models.signals import pre_delete
+from django.conf import settings
 from django.dispatch import receiver
 
 from django.contrib.auth import get_user_model
@@ -10,5 +11,5 @@ Account = get_user_model()
 
 @receiver(pre_delete, sender=Account)
 def unsubscribe_sns(sender, instance, **kwargs):
-    sns_client = boto3.client('sns')
+    sns_client = boto3.client('sns', region_name=settings.AWS_REGION)
     sns_client.unsubscribe(SubscriptionArn=instance.subscription_arn)
