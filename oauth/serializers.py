19c5e170083becb2883ac6a857c2e68e07b9d915
toluhunter <toluhunter19@gmail.com>
2023-04-16 10:42:00 +0100

    modified login

--- oauth/serializers.py
+++ oauth/serializers.py
@@ -31,31 +31,6 @@ class GoogleAuthSerializer(serializers.Serializer):
         return attrs
 
 
-class BookStoreAuthSerializer(serializers.Serializer):
-
-    code = serializers.CharField(max_length=100)
-
-    def validate(self, attrs):
-        url = "http://booksoreapi-env.eba-3igtf73b.us-east-1.elasticbeanstalk.com/oauth/token/"
-        token_response = requests.post(url=url, data={
-            'code': attrs["code"],
-            'client_id': settings.BOOK_STORE_CLIENT_ID,
-            'client_secret': settings.BOOK_STORE_CLIENT_SECRET,
-            'code_verifier': settings.BOOK_STORE_CODE_VERIFIER,
-            'redirect_uri': settings.BOOK_STORE_REDIRECT_URL,
-            'grant_type': 'authorization_code'
-        })
-
-        if token_response.status_code != 200:
-            # Return an error response if token exchange fails
-            raise serializers.ValidationError(
-                f'Failed to exchange authorization code for token')
-
-        attrs["token_res"] = token_response
-
-        return attrs
-
-
 class WalletAuthSerializer(serializers.Serializer):
 
     code = serializers.CharField(max_length=100)
